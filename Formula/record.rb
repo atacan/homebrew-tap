bb6e1f8e2e047d02f3df57ef15e233fc08ca2058
atacan <atacandur@icloud.com>
2026-02-08 20:38:02 +0100

    record 0.2.1

--- Formula/record.rb
+++ Formula/record.rb
@@ -1,17 +1,17 @@
 class Record < Formula
   desc "Record audio, screen, or camera output from the terminal"
   homepage "https://github.com/atacan/record"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/atacan/record/releases/download/v0.2.0/record-0.2.0-macos-arm64.tar.gz"
-      sha256 "74bc377749d98e315972f40eb60162b1dfb6bacff29a5afa670e5a43750b4d4e"
+      url "https://github.com/atacan/record/releases/download/v0.2.1/record-0.2.1-macos-arm64.tar.gz"
+      sha256 "83ef7445bba7732c4a148182c2e9e9723191a6c2b1d31916b29ef6fdd6ae2ab3"
     end
     on_intel do
-      url "https://github.com/atacan/record/releases/download/v0.2.0/record-0.2.0-macos-amd64.tar.gz"
-      sha256 "fdaff38529afcb8a9beda29b68322150ae8ff3bc44274c04cdb05ae94fcfdc59"
+      url "https://github.com/atacan/record/releases/download/v0.2.1/record-0.2.1-macos-amd64.tar.gz"
+      sha256 "28943f809c3d7d65d3bbaa939bd282ba1e43f1790388a0abd9a70e22094dc5ed"
     end
   end
 
